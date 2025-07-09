8cae1f1911f6d5bbdf5503947ddae871a68654b2
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-07-08 23:09:03 -0500

    Create Project Structure
    Created a project structure with configuration
    files and a backend. Will do frontend down the
    line. Code still doesn't run due to lack of
    database connectivity.

---
+++ backend/src/main/java/com/leoandfrick/catsm/CatsmApplication.java
@@ -0,0 +1,13 @@
+package com.leoandfrick.catsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/leoandfrick/catsm/CatsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoandfrick.catsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
